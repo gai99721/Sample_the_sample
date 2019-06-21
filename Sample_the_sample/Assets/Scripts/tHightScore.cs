58804f1cc107b8336924c1f8f74bde56f41eaa20
DA\nwuser <kgai.yda@gmail.com>
2019-06-21 11:48:02 +0900

    add_ScoreClass

---
+++ Sample_the_sample/Assets/Scripts/tHightScore.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NCMB;
+
+namespace NCMB
+{
+    public class tHightScore
+    {
+
+        public int score { get; set; }
+        public string name { get; private set; }
+
+        // コンストラクタ
+        public tHightScore(int _score, string _name)
+        {
+            score = _score;
+            name = _name;
+        }
+
+        // サーバーにハイスコアを保存
+        public void save()
+        {
+            // データストアの「HighScore」クラスから、Nameをキーにして検索
+            NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject>("HighScore");
+            query.WhereEqualTo("Name", name);
+            query.FindAsync((List<NCMBObject> objList, NCMBException e) =>
+            {
+
+                //検索成功したら
+                if (e == null)
+                {
+                    objList[0]["Score"] = score;
+                    objList[0].SaveAsync();
+                }
+            });
+        }
+
+        // サーバーからハイスコアを取得
+        public void fetch()
+        {
+            // データストアの「HighScore」クラスから、Nameをキーにして検索
+            NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject>("HighScore");
+            query.WhereEqualTo("Name", name);
+            query.FindAsync((List<NCMBObject> objList, NCMBException e) =>
+            {
+
+                //検索成功したら
+                if (e == null)
+                {
+                    // ハイスコアが未登録だったら
+                    if (objList.Count == 0)
+                    {
+                        NCMBObject obj = new NCMBObject("HighScore");
+                        obj["Name"] = name;
+                        obj["Score"] = 0;
+                        obj.SaveAsync();
+                        score = 0;
+                    }
+                    // ハイスコアが登録済みだったら
+                    else
+                    {
+                        score = System.Convert.ToInt32(objList[0]["Score"]);
+                    }
+                }
+            });
+        }
+        public string print()
+        {
+            return name + ' ' + score;
+        }
+    }
+}
---
+++ Sample_the_sample/Assets/Scripts/tScore.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NCMB;
+
+public class tScore : MonoBehaviour {
+
+    // スコアを表示するGUIText
+    public GUIText scoreGUIText;
+
+    // ハイスコアを表示するGUIText
+    public GUIText highScoreGUIText;
+
+    // スコア
+    public float t_Score;
+
+    // ハイスコア
+    //private int highScore;
+
+    private NCMB.tHightScore highScore;
+    private bool isNewRecord;
+
+    // PlayerPrefsで保存するためのキー
+    //private string highScoreKey = "highScore";
+
+    void Start()
+    {
+        Initialize();
+
+        // ハイスコアを取得する。保存されてなければ0点。
+        string name = FindObjectOfType<UserAuth>().currentPlayer();
+        highScore = new NCMB.tHightScore(0, name);
+        highScore.fetch();
+    }
+
+    void Update()
+    {
+        // スコアがハイスコアより大きければ
+        if (highScore.score < t_Score)
+        {
+            isNewRecord = true;
+            highScore.score = (int)t_Score;
+        }
+
+        // スコア・ハイスコアを表示する
+        scoreGUIText.text = t_Score.ToString();
+        highScoreGUIText.text = "HighScore : " + highScore.score.ToString();
+    }
+
+    // ゲーム開始前の状態に戻す
+    private void Initialize()
+    {
+        // スコアを0に戻す
+        t_Score = 0;
+        //SceneManager.LoadScene("LeaderBoard");
+        // ハイスコアを取得する。保存されてなければ0を取得する。
+        isNewRecord = false;
+    }
+
+    // ポイントの追加
+    public void AddPoint(int point)
+    {
+        t_Score = t_Score + point;
+    }
+
+    // ハイスコアの保存
+    public void Save()
+    {
+        // ハイスコアを保存する（ただし記録の更新があったときだけ）
+        if (isNewRecord)
+        {
+            highScore.save();
+        }
+
+        // ゲーム開始前の状態に戻す
+        Initialize();
+    }
+}
